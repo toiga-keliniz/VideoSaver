dfb46ab50cd82899560aad338dedfaf937fc5120
NebulaRift <bekabd969@gmail.com>
2024-08-26 22:14:43 +0600

    commit

--- language-redirect.js
+++ language-redirect.js
@@ -3,8 +3,8 @@ function checkAndRedirect() {
         var userLang = navigator.language || navigator.userLanguage;
         if (userLang.startsWith('ru') && !window.location.pathname.includes('/ru/')) {
             window.location.href = "/ru/index.html";
-        } else if (!userLang.startsWith('ru') && !window.location.pathname.includes('/en/')) {
-            window.location.href = "/en/index.html";
+        } else if (!userLang.startsWith('ru') && !window.location.pathname.includes('/')) {
+            window.location.href = "/index.html";
         }
         localStorage.setItem('languageSelected', 'true');
     }
